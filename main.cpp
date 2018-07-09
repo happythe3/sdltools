0f9f534b9eff5c0ed4f4c029caff1b504b9b723e
happythe3 <alexshearer00@gmail.com>
2018-07-09 17:25:09 +0800

    Added license to all files

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+#include "examples\example1.h"
+
+using namespace std;
+
+int main(int argc, char* args[])
+{
+	cout << "sdltools examples" << endl;
+
+	example1();
+
+	return 0;
+}
+
